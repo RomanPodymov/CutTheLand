2ddd2a017ff66808010d89d2f9daa04505ec4f94
Roman Podymov <podymfrombryansk@yandex.com>
2024-07-07 19:25:14 +0200

    Build params

---
+++ build_settings/build_settings.lua
@@ -0,0 +1,10 @@
+local params = { 
+   platform='html5', 
+   appName='Cut_The_Land', 
+   appVersion='1.0.0', 
+   dstPath='../', 
+   projectPath='../', 
+   includeStandardResources = 'true', 
+} 
+
+return params
